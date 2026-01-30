dd56da2dbd32e9e6737ef77732a4f0ffae139459
Sean Dela Cruz <sean.delacruz@128techconsultinginc.com>
2026-01-30 18:00:04 +0800

    try commit push

--- Infrastructure/Repositories/Import/ImportOvertimeApplicationRepository.cs
+++ Infrastructure/Repositories/Import/ImportOvertimeApplicationRepository.cs
@@ -1,19 +1,14 @@
 ﻿using DomainEntities.Dto;
-using DomainEntities.DTO.FileSetUp.Employment;
 using Infrastructure.IRepositories.Import;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Timekeeping.Infrastructure.Data;
 
 namespace Infrastructure.Repositories.Import;
 
-public class ImportOvertimeApplicationRepository : IImportOvertimeApplicationRepository
+public class ImportOvertimeApplicationRepository(TimekeepingContext context) : IImportOvertimeApplicationRepository
 {
-    private readonly TimekeepingContext _context;
-    public ImportOvertimeApplicationRepository(TimekeepingContext context)
-    {
-        _context = context;
-    }
+    private readonly TimekeepingContext _context = context;
+
     public async Task UpdateImportOvertimeApplication(ImportOvertimeApplicationDto overtimeApplicationDto)
     {
         await _context.Database.ExecuteSqlInterpolatedAsync
